6ae61c08fb9aeef5b44d028d04a9e3b0b728ab2f
Yakub Jorayev <jorayev2494@gmail.com>
2026-02-02 13:13:07 +0500

    roter clear

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AIController;
 
-// Route::view('/{any?}', 'index')->where('any', '[\/\w\.-]*');
 Route::get('/ai/assistants/{slug?}', [AIController::class, 'index'])
-    // ->where('slug', '[0-9]+')
+    ->where('slug', '[a-z]+')
     ->name('assistant');
 
 Route::post('/ai/assistants/{slug}', [AIController::class, 'send'])
-    // ->where('slug', '[0-9]+')
+    ->where('slug', '[a-z]+')
     ->name('assistant.send');
 
 
